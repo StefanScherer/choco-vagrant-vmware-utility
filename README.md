5f30cc6e3fd630e4d280b21b2eb17f39ff5758d2
Stefan Scherer <scherer_stefan@icloud.com>
2018-03-27 22:35:59 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,15 @@
+# chocolatey installer for Vagrant VMware Utility
+
+[![Latest version released](https://img.shields.io/chocolatey/v/vagrant-vmware-utility.svg)](https://chocolatey.org/packages/vagrant-vmware-utility)
+[![Package downloads count](https://img.shields.io/chocolatey/dt/vagrant-vmware-utility.svg)](https://chocolatey.org/packages/vagrant-vmware-utility)
+[![Build status](https://ci.appveyor.com/api/projects/status/2h4xf63mmnxg3yyi/branch/master?svg=true)](https://ci.appveyor.com/project/StefanScherer/choco-vagrant-vmware-utility/branch/master)
+
+This is a chocolatey package for the Vagrant VMware Utility. Please see MAINTENANCE.md
+for information on how to upkeep it.
+
+To use, simply run `choco install vagrant-vmware-utility -y` (this may need a `-pre`
+flag also if it hasn't gone through moderation).
+
+# Links
+
+* https://www.vagrantup.com/vmware/downloads.html
---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,35 @@
+$url64 = 'https://releases.hashicorp.com/vagrant-vmware-utility/0.9.9/vagrant-vmware-utility_0.9.9_x86_64.msi'
+$checksum64 = '24140831596cd8ce5be3ba8205cdd7fc8ebd6ebffe7dde445249b6ab82ab8bff'
+$checksumType64 = $checksumType
+$url = $url64
+$checksum = $checksum64
+$checksumType = $checksumType64
+
+$legacyLocation = "$env:SystemDrive\HashiCorp\packer"
+$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+if ([System.IO.Directory]::Exists("$env:ChocolateyInstall\lib\packer")) {
+  if ([System.IO.Directory]::Exists("$env:ChocolateyInstall\lib\packer\tools")) {
+    # clean old plugins and ignore files
+    Write-Host "Removing old packer plugins"
+    Remove-Item "$env:ChocolateyInstall\lib\packer\tools\packer-*.*"
+  }
+} else {
+  if ([System.IO.Directory]::Exists("$env:ALLUSERSPROFILE\chocolatey\lib\packer")) {
+    if ([System.IO.Directory]::Exists("$env:ALLUSERSPROFILE\chocolatey\lib\packer\tools")) {
+      # clean old plugins and ignore files
+      Write-Host "Removing old packer plugins"
+      Remove-Item "$env:ALLUSERSPROFILE\chocolatey\lib\packer\tools" -Include "packer-*.*"
+    }
+  }
+}
+
+$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage "packer" "$url" "$unzipLocation" "$url64" `
+ -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64
+
+If (Test-Path $legacyLocation) {
+  Write-Host "Removing old packer installation from $legacyLocation"
+  Remove-Item $legacyLocation -Force -Recurse
+}
